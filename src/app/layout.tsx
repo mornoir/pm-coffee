690c252e47c12ac7f16810287e71524052641724
CWX TML <cwxtml@gmail.com>
2025-09-13 05:06:24 +0000

    change the theme to darkmode

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -28,7 +28,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" suppressHydrationWarning className="scroll-smooth">
+    <html lang="en" suppressHydrationWarning className="scroll-smooth dark">
       <body
         className={cn(
           'min-h-screen bg-background font-body antialiased',
