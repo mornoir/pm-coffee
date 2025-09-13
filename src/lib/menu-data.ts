b8a511321e45a6eb55e019618c891ba2ba0f2f81
CWX TML <cwxtml@gmail.com>
2025-09-13 10:06:48 +0000

    Please choose better placeholder images here (_for element img_)

--- src/lib/menu-data.ts
+++ src/lib/menu-data.ts
@@ -14,7 +14,7 @@ export const menuItems: MenuItem[] = [
         name: "Americano",
         price: "Rp 30.000",
         description: "A rich and intense shot of espresso, diluted with hot water.",
-        imageUrl: "https://images.unsplash.com/photo-1517959105821-eaf2591984ca?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w3NDE5ODJ8MHwxfHNlYXJjaHw5fHxhbWVyaWNhbm98ZW58MHx8fHwxNzU3NzQ4MjY4fDA&ixlib=rb-4.1.0&q=80&w=1080",
+        imageUrl: "https://images.pexels.com/photos/1684177/pexels-photo-1684177.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
         tags: ["coffee"]
     },
     {
@@ -22,7 +22,7 @@ export const menuItems: MenuItem[] = [
         name: "Latte",
         price: "Rp 35.000",
         description: "Smooth espresso with steamed milk, topped with a light layer of foam.",
-        imageUrl: "https://images.unsplash.com/photo-1497636577773-f1231844b336?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w3NDE5ODJ8MHwxfHNlYXJjaHwxOHx8bGF0dGV8ZW58MHx8fHwxNzU3NzQ4Mzg0fDA&ixlib=rb-4.1.0&q=80&w=1080",
+        imageUrl: "https://images.pexels.com/photos/312418/pexels-photo-312418.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
         tags: ["coffee", "recommended"]
     },
     {
@@ -30,7 +30,7 @@ export const menuItems: MenuItem[] = [
         name: 'Cappuccino',
         price: 'Rp 35.000',
         description: "A classic blend of espresso, steamed milk, and a thick layer of creamy foam.",
-        imageUrl: "https://images.unsplash.com/photo-1572442388796-11668a65343d?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w3NDE5ODJ8MHwxfHNlYXJjaHw1fHxjYXBwdWNjaW5vfGVufDB8fHx8MTc1Nzc0ODI5N3ww&ixlib=rb-4.1.0&q=80&w=1080",
+        imageUrl: "https://images.pexels.com/photos/1727123/pexels-photo-1727123.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
         tags: ["coffee"]
     },
     {
@@ -38,7 +38,7 @@ export const menuItems: MenuItem[] = [
         name: "Espresso",
         price: "Rp 25.000",
         description: "A concentrated coffee brewed by forcing a small amount of nearly boiling water through finely-ground coffee beans.",
-        imageUrl: "https://images.unsplash.com/photo-1596707833141-547a4b3b2a2e?q=80&w=1287&auto=format&fit=crop",
+        imageUrl: "https://images.pexels.com/photos/4109848/pexels-photo-4109848.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
         tags: ["coffee"]
     },
     {
@@ -46,7 +46,7 @@ export const menuItems: MenuItem[] = [
         name: "Macchiato",
         price: "Rp 32.000",
         description: "An espresso coffee drink with a small amount of milk, usually foamed.",
-        imageUrl: "https://images.unsplash.com/photo-1571224230846-9636b1511059?q=80&w=1287&auto=format&fit=crop",
+        imageUrl: "https://images.pexels.com/photos/324030/pexels-photo-324030.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
         tags: ["coffee"]
     },
     {
@@ -54,7 +54,7 @@ export const menuItems: MenuItem[] = [
         name: "Mocha",
         price: "Rp 38.000",
         description: "A delightful blend of espresso, chocolate, and steamed milk.",
-        imageUrl: "https://images.unsplash.com/photo-1542990253-a7814f27a22c?q=80&w=1287&auto=format&fit=crop",
+        imageUrl: "https://images.pexels.com/photos/357283/pexels-photo-357283.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
         tags: ["coffee", "recommended"]
     },
     {
@@ -62,7 +62,7 @@ export const menuItems: MenuItem[] = [
         name: "Flat White",
         price: "Rp 35.000",
         description: "Espresso with steamed milk, consisting of a thin layer of microfoam.",
-        imageUrl: "https://images.unsplash.com/photo-1561882468-91866540424d?q=80&w=1287&auto=format&fit=crop",
+        imageUrl: "https://images.pexels.com/photos/982612/pexels-photo-982612.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
         tags: ["coffee"]
     },
     {
@@ -70,7 +70,7 @@ export const menuItems: MenuItem[] = [
         name: "Croissant",
         price: "Rp 25.000",
         description: "A buttery, flaky, and golden-brown pastry, perfect with coffee.",
-        imageUrl: "https://images.unsplash.com/photo-1599940778173-e276d4acb2bb?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w3NDE5ODJ8MHwxfHNlYXJjaHw0fHxjcm9pc3NhbnR8ZW58MHx8fHwxNzU3NzQ4NDM4fDA&ixlib=rb-4.1.0&q=80&w=1080",
+        imageUrl: "https://images.pexels.com/photos/2631613/pexels-photo-2631613.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
         tags: ["snack & desserts"]
     },
     {
@@ -78,7 +78,7 @@ export const menuItems: MenuItem[] = [
         name: "Avocado Toast",
         price: "Rp 55.000",
         description: "Smashed avocado on toasted sourdough, with a sprinkle of chili flakes.",
-        imageUrl: "https://images.unsplash.com/photo-1628556820645-63ba5f90e6a2?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w3NDE5ODJ8MHwxfHNlYXJjaHw2fHxhdm9jYWRvJTIwdG9hc3R8ZW58MHx8fHwxNzU3NzQ4NTMxfDA&ixlib=rb-4.1.0&q=80&w=1080",
+        imageUrl: "https://images.pexels.com/photos/6622177/pexels-photo-6622177.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
         tags: ["eatery", "recommended"]
     },
     {
@@ -86,7 +86,7 @@ export const menuItems: MenuItem[] = [
         name: 'Matcha Latte',
         price: 'Rp 40.000',
         description: "Earthy Japanese green tea powder whisked with frothy steamed milk.",
-        imageUrl: "https://images.unsplash.com/photo-1563822552-3200133AC489?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w3NDE5ODJ8MHwxfHNlYXJjaHwzfHxtYXRjaGElMjBsYXR0ZXxlbnwwfHx8fDE3NTc3NDg0ODF8MA&ixlib=rb-4.1.0&q=80&w=1080",
+        imageUrl: "https://images.pexels.com/photos/2332686/pexels-photo-2332686.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
         tags: ["non-coffee", "recommended"]
     },
     {
@@ -94,7 +94,7 @@ export const menuItems: MenuItem[] = [
         name: 'Iced Chocolate',
         price: 'Rp 38.000',
         description: "Rich, decadent chocolate chilled with milk and ice. A refreshing treat.",
-        imageUrl: "https://images.unsplash.com/photo-1562228122-29135a585f83?q=80&w=1287&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        imageUrl: "https://images.pexels.com/photos/1342468/pexels-photo-1342468.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
         tags: ["non-coffee"]
     },
     {
@@ -102,7 +102,7 @@ export const menuItems: MenuItem[] = [
         name: 'Iced Tea',
         price: 'Rp 25.000',
         description: "Freshly brewed tea, served chilled over ice with a slice of lemon.",
-        imageUrl: "https://images.unsplash.com/photo-1556745753-1f3c5fbe6b3c?q=80&w=1287&auto=format&fit=crop",
+        imageUrl: "https://images.pexels.com/photos/141014/pexels-photo-141014.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
         tags: ["non-coffee"]
     },
     {
@@ -110,7 +110,7 @@ export const menuItems: MenuItem[] = [
         name: 'Fruit Smoothie',
         price: 'Rp 45.000',
         description: "A refreshing blend of mixed berries, banana, and yogurt.",
-        imageUrl: "https://images.unsplash.com/photo-1610970881699-44a5c8a23486?q=80&w=1287&auto=format&fit=crop",
+        imageUrl: "https://images.pexels.com/photos/4112863/pexels-photo-4112863.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
         tags: ["non-coffee"]
     },
     {
@@ -118,7 +118,7 @@ export const menuItems: MenuItem[] = [
         name: 'Artisan Tea',
         price: 'Rp 28.000',
         description: "A selection of hand-picked, loose-leaf teas with unique flavor profiles.",
-        imageUrl: "https://images.unsplash.com/photo-1563822552-3200133AC489?q=80&w=1287&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        imageUrl: "https://images.pexels.com/photos/39347/tea-tea-leaves-herbal-tea-flavor-39347.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
         tags: ["non-coffee"]
     },
     {
@@ -126,7 +126,7 @@ export const menuItems: MenuItem[] = [
         name: 'Quinoa Salad',
         price: 'Rp 65.000',
         description: "A healthy and vibrant mix of quinoa, fresh vegetables, and a lemon vinaigrette.",
-        imageUrl: "https://images.unsplash.com/photo-1551248429-4097c682c478?q=80&w=1287&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        imageUrl: "https://images.pexels.com/photos/1152237/pexels-photo-1152237.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
         tags: ["eatery"]
     },
     {
@@ -134,7 +134,7 @@ export const menuItems: MenuItem[] = [
         name: 'Club Sandwich',
         price: 'Rp 75.000',
         description: "A triple-decker sandwich with grilled chicken, bacon, lettuce, tomato, and mayo.",
-        imageUrl: "https://images.unsplash.com/photo-1592415486699-8d5a71727349?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w3NDE5ODJ8MHwxfHNlYXJjaHwzfHxjbHViJTIwc2FuZHdpY2h8ZW58MHx8fHwxNzU3NzQ4NjU3fDA&ixlib=rb-4.1.0&q=80&w=1080",
+        imageUrl: "https://images.pexels.com/photos/1600727/pexels-photo-1600727.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
         tags: ["eatery", "recommended"]
     },
     {
@@ -142,7 +142,7 @@ export const menuItems: MenuItem[] = [
         name: 'Cheesecake Slice',
         price: 'Rp 45.000',
         description: "Creamy, rich, and decadent New York-style cheesecake with a graham cracker crust.",
-        imageUrl: "https://images.unsplash.com/photo-1565792595381-32547a4635ee?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w3NDE5ODJ8MHwxfHNlYXJjaHw1fHxjaGVlc2VjYWtlJTIwc2xpY2V8ZW58MHx8fHwxNzU3NzQ4NjI2fDA&ixlib=rb-4.1.0&q=80&w=1080",
+        imageUrl: "https://images.pexels.com/photos/1126359/pexels-photo-1126359.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
         tags: ["snack & desserts", "recommended"]
     },
     {
@@ -150,9 +150,7 @@ export const menuItems: MenuItem[] = [
         name: 'Giant Choco Cookie',
         price: 'Rp 30.000',
         description: "A warm, soft-baked giant cookie loaded with gooey chocolate chips.",
-        imageUrl: "https://images.unsplash.com/photo-1598839958373-c15496a76e93?q=80&w=1287&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        imageUrl: "https://images.pexels.com/photos/2067396/pexels-photo-2067396.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
         tags: ["snack & desserts"]
     },
 ];
-
-    
